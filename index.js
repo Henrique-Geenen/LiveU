76b1b87acf7ede0a0687c06a300a2ca425b39c33
Henrique-Geenen <henriqueggo@hotmail.com>
2021-02-26 23:49:47 -0300

    entrega final

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-const http = require('http');
+const express = require('express');
+const app = express ();
+const path = require('path');
 
-// createServer method takes a callback function as argument
-// the callback function takes two arguments req and re
-const server = http.createServer(function (req, res) {
-    res.statusCode = 200; // 200 = OK
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>Demo page</h1>");
-    res.end();
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "http://127.0.0.1:5500/LiveU/");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    next()
+  });
+
+app.use(express.static('C:/projects/LiveU'));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-// server is listening to incoming requests on port 3000 on localhost
-server.listen(3000, function () {
-    console.log("Listening on port http://localhost:3000");
+app.post('/index', function(req,res){
+
+const sql = require('mssql');
+
+const config = {
+    user: 'user_trial',
+    password: '7412LIVE!@#$%¨&*()',
+    server: 'virtual2.febracorp.org.br',
+    database: 'CONTOSO',
+    port: 1433,
+    
+    options: {
+        cryptoCredentialsDetails: {
+          minVersion: 'TLSv1'
+        }
+      }
+ };
+
+sql.connect(config, function(err){
+
+    if (err) console.log(err);
+
+    let sqlRequest = new sql.Request();
+
+    let sqlQuery = 'use CONTOSO; INSERT INTO tbs_nome (nome, cod) VALUES('+ localStorage.nome +', '+ localStorage.codNome +'); INSERT INTO tbs_sobrenome (nome, cod) VALUES('+ localStorage.sobrenome +', '+ localStorage.codSobrenome +'); INSERT INTO tbs_email (nome, cod) VALUES('+ localStorage.email +', '+ localStorage.codEmail +');';
+    
+    sqlRequest.query(sqlQuery, function(err, data){
+
+        if (err) console.log (err)
+
+        console.log(data);
+        console.log(data.recordset);
+        console.log(data.rowsAffected);
+        console.log(data.recordset[0]);
+
+    sql.close();
+
+    });
+});
+});
+
+
+const webserver = app.listen(5000, function(){
+    console.log('Node Web Server is running..');
 });
--- js/post.js
+++ js/post.js
@@ -1,17 +1,21 @@
 var enviar = document.querySelector("#enviar");
+var xhttp = new XMLHttpRequest();
 
 enviar.addEventListener("click", function() {
-
     event.preventDefault();
 
-    var nome = document.querySelector("#nome").value;
-    var sobrenome = document.querySelector("#sobrenome").value;
-    var email = document.querySelector("#email").value;
+    localStorage.nome = document.querySelector("#nome").value;
+    localStorage.sobrenome = document.querySelector("#sobrenome").value;
+    localStorage.email = document.querySelector("#email").value;
+
+    // document.getElementById('nomeRes').innerHTML = localStorage.nome
+    // document.getElementById('sobrenomeRes').innerHTML = localStorage.sobrenome
+    // document.getElementById('emailRes').innerHTML = localStorage.email
 
     var user =  {
-        nome: nome,
-        sobrenome: sobrenome,
-        email: email
+        nome: localStorage.nome,
+        sobrenome: localStorage.sobrenome,
+        email: localStorage.email
     };
 
     console.log(user);
@@ -26,18 +30,19 @@ enviar.addEventListener("click", function() {
             console.log(data)
 
             var codigos = data.split("#")
-            var codNome = codigos[1]
-            var codSobrenome = codigos[3]
-            var codEmail = codigos[5]
+            localStorage.codNome = codigos[1]
+            localStorage.codSobrenome = codigos[3]
+            localStorage.codEmail = codigos[5]
             
-            console.log(codNome)
-            console.log(codSobrenome)
-            console.log(codEmail)
+            console.log(localStorage.codNome)
+            console.log(localStorage.codSobrenome)
+            console.log(localStorage.codEmail)
 
         },
         fail: function(jqXHR, textStatus) {
             console.log("fail")
             alert("Request failed: " + textStatus);
-        }
+        },
     });
+    // document.getElementById("form-res").submit();
 });
